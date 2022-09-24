bd4b6498bdc049f9fa013faca5f0310fd1ff61e2
MhmmYah <yew.m.loy@gmail.com>
2022-09-24 00:24:11 -0400

    Finalized functionality and nice intro page.

--- javascript/rockpaperscissors.js
+++ javascript/rockpaperscissors.js
@@ -1,49 +1,98 @@
+const display = document.getElementById("gameresult");
+const pscore = document.getElementById("playerscore");
+const tscore = document.getElementById("tie");
+const cscore = document.getElementById("computerscore");
 const gamedict = ["rock", "paper", "scissors"];
+let pW = 0;
+let cW = 0;
+let tie = 0;
 
 function getComputerChoice(){
     return Math.floor(Math.random()*3)+1;
 }
 
-function getUserChoice(){
-    let answer = prompt("Choose rock, paper, or scissors", "rock").toLowerCase();
+function getUserChoice(answer){
     return gamedict.indexOf(answer)+1;
 }
 
+function removeContent(element){
+    var removecontent = element.querySelectorAll('*');
+    removecontent.forEach((thing) => {element.removeChild(thing)});
+}
+
+function addContent(string, tag, element){
+    var content = document.createElement(tag);
+    content.textContent = string;
+    element.appendChild(content);
+}
+
 function playAGame(playerSelection, computerSelection){
+    var content = document.createElement('h1');
     let result = (((computerSelection - playerSelection)%3)+3)%3;
+    removeContent(display);
     if(result == 1){
-        console.log("computer wins");
+        addContent("Dang!","h1",display);
         return -1;
     }
     else if (result == 0) {
-        console.log("tied");
+        addContent("Again!","h1",display);
         return 0;
     }
     else {
-        console.log("user wins");
+        addContent("Nice Play!","h1",display);
         return 1;
     }
 }
 
-function runGame(){
-    let pW = 0;
-    let cW = 0;
-    let tie = 0;
-    for(let i = 0; i < 5; i++){
-        let pS = getUserChoice();
-        let cS = getComputerChoice();
-        console.log("Player chose: " + gamedict[pS-1]);
-        console.log("Computer chose: " + gamedict[cS-1]);
-        let outcome = playAGame(pS,cS);
-        if(outcome == 1){
-            pW += 1;
-        } else if (outcome == 0){
-            cW += 1;
-        }else{
-            tie +=1;
-        }
+function runGame(pvalue){
+    cS = getComputerChoice();
+    let outcome = playAGame(getUserChoice(pvalue),cS);
+    if(outcome == 1){
+        pW += 1;
+        removeContent(pscore);
+        addContent(pW,"h2",pscore)
+    } else if (outcome == 0){
+        cW += 1;
+        removeContent(cscore);
+        addContent(cW,"h2",cscore)
+    }else{
+        tie +=1;
+        removeContent(tscore);
+        addContent(tie,"h2",tscore)
     }
-    console.log(pW + " " + cW + " " + tie);
+    if(pW == 5 || cW == 5){
+        removeContent(display);
+        if(pW == 5){
+            addContent("You Win!","h1",display);
+        }else{
+            addContent("Computer Wins!","h1",display);
+        }
+        buttons.forEach((button)=>{button.style.visibility ='hidden';});
+        return 0;
+    }
+    console.log(pW, cW, tie)
 }
 
-runGame();
+function resetGame(){
+    pW = cW = tie = 0;
+    removeContent(display);
+    removeContent(pscore);
+    removeContent(tscore);
+    removeContent(cscore);
+    addContent("Begin Game!","h1",display);
+    addContent(pW,"h2",pscore);
+    addContent(cW,"h2",tscore);
+    addContent(tie,"h2",cscore);
+    buttons.forEach((button)=>{button.style.visibility ='visible';});
+}
+
+const buttons = document.querySelectorAll('button.play');
+buttons.forEach((button)=>{
+    button.addEventListener('click',() => {
+        runGame(button.id);
+    })
+});
+const reset = document.querySelector('button.reset');
+reset.addEventListener('click', () => {
+    resetGame();
+});
